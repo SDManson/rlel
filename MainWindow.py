711fabf85bb22d03d495d31e8912d5a074513bbe
frosty-nee <frostbite@frosty-nee.net>
2018-07-28 00:57:34 -0400

    tq path discovery

--- MainWindow.py
+++ MainWindow.py
@@ -1,4 +1,5 @@
 import wx
+import os
 
 class MainWindow(wx.Frame):
 	def __init__(self, parent, title):
@@ -6,21 +7,21 @@ class MainWindow(wx.Frame):
 			style=wx.DEFAULT_FRAME_STYLE & ~wx.RESIZE_BORDER)
 		self.SetBackgroundColour("white")
 
-		eve_path = wx.TextCtrl(self, wx.ID_ANY,
-			 "path to eve install root", pos=wx.Point(12,12), size=wx.Size(220,23))
-		singularity_radio = wx.CheckBox(self, wx.ID_ANY, pos=wx.Point(242, 12), label="Singularity")
-		eve_path_browse_button = wx.Button(self, wx.ID_ANY, "Browse",
-			pos=singularity_radio.Position + wx.Size(singularity_radio.Size.width,0) + wx.Point(12,0),
-			size=wx.Size(70,23))
-		self.account_box = wx.ListBox(self, wx.ID_ANY, pos=wx.Point(12, 48), size=wx.Size(300,280),
-		choices=["character", "select", "goes", "here"],
-		style=wx.LB_MULTIPLE)
-		self.save_button = wx.Button(self, wx.ID_ANY, "Save", size=wx.Size(88, 23), pos=wx.Point(318,48))
-		self.launch_button = wx.Button(self, wx.ID_ANY, "Launch", size=wx.Size(88,23), pos=wx.Point(318,0)+(wx.Point(0,self.save_button.Position.y + self.save_button.Size.height + 2)))
+		self.eve_path = wx.DirPickerCtrl(self, wx.ID_ANY, style=wx.FLP_USE_TEXTCTRL,
+			pos=wx.Point(12,12), size=wx.Size(310,23))
+		self.eve_path.TextCtrl.Size=wx.Size(240,23)
+		self.eve_path.PickerCtrl.Position=wx.Point(240,0)
+		self.eve_path.PickerCtrl.Size=wx.Size(70,23)
+		#self.singularity_radio = wx.CheckBox(self, wx.ID_ANY, pos=wx.Point(242, 12), label="Singularity")
+		self.account_box = wx.ListBox(self, wx.ID_ANY, pos=wx.Point(12, 70), size=wx.Size(300,280),
+			choices=["character", "select", "goes", "here"],
+			style=wx.LB_MULTIPLE)
+		self.launch_button = wx.Button(self, wx.ID_ANY, "Launch", size=wx.Size(88,23), pos=wx.Point(318,48))
 		self.add_char_button = wx.Button(self, wx.ID_ANY, "Add Character", size=wx.Size(88,23),  pos=wx.Point(318,0)+(wx.Point(0,self.launch_button.Position.y + self.launch_button.Size.height + 2)))
 		self.remove_char_button = wx.Button(self, wx.ID_ANY, "Remove Char", size=wx.Size(88,23),  pos=wx.Point(318,0)+(wx.Point(0,self.add_char_button.Position.y + self.add_char_button.Size.height + 2)))
 		self.SetIcon(wx.Icon('rlel.ico'))
 		self.Bind(wx.EVT_BUTTON, self.remove_char, id=self.remove_char_button.GetId())
+		self.eve_path.SetPath(self.get_tranq_path())
 		self.Show(True)
 
 	def remove_char(self, event):
@@ -30,3 +31,17 @@ class MainWindow(wx.Frame):
 			i+=1
 		return
 	
+	def get_tranq_path(self):
+		path = None
+		appdata = os.getenv('LOCALAPPDATA')
+		for d in os.listdir(os.path.join(appdata,'ccp','eve')):
+			if '_tranquility' in d:
+				split = d.split('_')
+				if split[-1] == 'tranquility':
+					drive = split[0]
+					drive = str.capitalize(drive) + ':' + os.sep
+					path = os.path.join(drive, *split[1:-1])			
+					return path
+		return ''
+
+
